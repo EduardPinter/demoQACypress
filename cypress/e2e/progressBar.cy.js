f1311a807a0054f1b67bcabe0194c2ad8fb6c1b9
EduardPinter <edi.bionicle@gmail.com>
2023-11-22 15:00:18 +0100

    progress bar tests

--- cypress/e2e/progressBar.cy.js
+++ cypress/e2e/progressBar.cy.js
@@ -1,12 +1,42 @@
 /// <reference types="Cypress" />
 
-describe('template spec', () => {
-  it('Check start stop progress', () => {
+import { progressBarPage } from "../page-objects/progressBarPage"
+
+describe('Progress Bar', () => {
+
+
+  it.only('Check start stop progress', () => {
     cy.visit('https://demoqa.com/progress-bar')
     Cypress.on('uncaught:exception', (err, runnable) => { 
       return false
   })
-    cy.contains("Start").click();
-    
+    progressBarPage.clickStartStopButton();
+    progressBarPage.checkProgressBarValueNumber(25);
+    progressBarPage.clickStartStopButton();
+    progressBarPage.checkProgressBarValueNumber(25);
+    progressBarPage.clickStartStopButton();
+    progressBarPage.checkProgressBarValueNumber(50);
+    progressBarPage.clickStartStopButton();
+  })
+
+  it('Check for full bar', () => {
+    cy.visit('https://demoqa.com/progress-bar')
+    Cypress.on('uncaught:exception', (err, runnable) => { 
+      return false
+  })
+    progressBarPage.clickStartStopButton();
+    progressBarPage.checkProgressBarValueNumberWithTimeout(12000,100);
+  })
+
+  it('Check the reset button functionality', () => {
+    cy.visit('https://demoqa.com/progress-bar')
+    Cypress.on('uncaught:exception', (err, runnable) => { 
+      return false
+  })
+    progressBarPage.clickStartStopButton();
+    progressBarPage.checkProgressBarValueNumberWithTimeout(12000,100);
+    progressBarPage.clickResetBtn();
+    //progressBarPage.checkProgressBarValueNumber(0);
+
   })
 })
--- cypress/page-objects/progressBarPage.js
+++ cypress/page-objects/progressBarPage.js
@@ -3,14 +3,23 @@ class ProgressBarPage {
     constructor(){
         this.startStopButton = "#startStopButton";
         this.progressBar="div[id='progressBar']>div";
+        this.resetBtn= "#resetButton";
     }
 
 clickStartStopButton(){
     cy.get(this.startStopButton).click();
 }
 
-checkProgressBarValueNumber(eq ,number){
-    cy.get(this.progressBar).should('have.attr', 'aria-valuenow').and(eq, number)
+checkProgressBarValueNumber(number){
+    cy.get(this.progressBar).should('have.attr', 'aria-valuenow', number)
+}
+
+checkProgressBarValueNumberWithTimeout(timeoutNum, number){
+    cy.get(this.progressBar, { timeout: timeoutNum }).should('have.attr', 'aria-valuenow', number)
+}
+
+clickResetBtn(){
+    cy.get(this.resetBtn).click();
 }
 
 }
